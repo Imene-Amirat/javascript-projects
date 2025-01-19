4c8a4810a3bc8ae65694d10ef04448b9cf43d9e7
Imene-Amirat <imene2004.amirat@gmail.com>
2025-01-19 20:33:28 +0100

    Fix price formatting and radio selectors

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,11 +1,18 @@
-export const cart = [];
+export const cart = [{
+    productId: '54e0eccd-8f36-462b-b68a-8182611d9add',
+    quantity: 1
+}, {
+    productId: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
+    quantity: 1
+}];
 
 export function addToCart(productId) {
     let matchingItem;
-
+    
     cart.forEach((cartItem) => {
-        if(productId === cartItem.productId)
+        if(productId === cartItem.productId){
             matchingItem = cartItem;
+        }
     })
 
     if(matchingItem)
@@ -17,4 +24,3 @@ export function addToCart(productId) {
         });
     }
 };
-console.log(cart);
