3ef54055de5c9b98e6320935d3e5036ccef18ca0
Imene-Amirat <imene2004.amirat@gmail.com>
2025-01-30 11:56:57 +0100

    Convert load products from XHR to fetch

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,14 +1,10 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import {loadProducts} from '../data/products.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-new Promise((resolve) => {
-    loadProducts(() => {
-        resolve();
-    });
-}).then(() => {
+loadProductsFetch().then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
